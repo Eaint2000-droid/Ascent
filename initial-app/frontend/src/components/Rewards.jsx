315b6238d21cd948b000456c12cdd4c854b7d74e
yuenkm40 <yuenkm40@gmail.com>
2022-11-02 21:44:00 +0800

    minor edit to landing

--- initial-app/frontend/src/components/Rewards.jsx
+++ initial-app/frontend/src/components/Rewards.jsx
@@ -17,9 +17,7 @@ const Rewards = () => (
         Enjoy rewards from <br className="sm:block hidden" /> a variety of card programs.
       </h2>
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-        Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio
-        aenean neque. Fusce ipsum orci rhoncus aliporttitor integer platea
-        placerat.
+        Earn points, cashback and miles from card programs like SCIS Shopping, SCIS Freedom, SCIS PremiumMiles & SCIS PlatinumMiles.
       </p>
 
       <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
