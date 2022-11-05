6dfa86bdf3308960510720d4ef13aedd240232f7
JieEnT <jieen1000@gmail.com>
2022-11-05 17:14:40 +0800

    fix: changed Home recursive call

--- initial-app/frontend/src/components/Home/Home.jsx
+++ initial-app/frontend/src/components/Home/Home.jsx
@@ -8,7 +8,6 @@ import Banner from "./Banner"
 import Navbar from '../Navbar'
 
 function Home() {
-  const [initialData, setInitialData] = useState([]);
   const [currentCampaign, setCurrentCampaign] = useState([]);
   
   const date = new Date();
@@ -20,49 +19,43 @@ function Home() {
   var username = localStorage.getItem('CognitoIdentityServiceProvider.4eid9s9q0khtii45ko77b20ijt.LastAuthUser');
 
 
-
-  //Load data
- useEffect(() => {
-     const sendRequest = async () => {
-     try{
+ const sendRequest = async () => {
          const response = await fetch('https://tfaz66806a.execute-api.ap-southeast-1.amazonaws.com/beta/v1', {
             headers: {
               "Authorization" : localStorage.getItem("jwtToken")
             }
-          });
-         const responseData = await response.json();
-         setInitialData(JSON.parse(responseData));
-         console.log(initialData);
+          })
+        return response.json();
+}
 
-         for (let i = 0; i< initialData.length;i++) {
-           var startdate_and_time = initialData[i][3].split(' '); 
-           var startdate_time_split = startdate_and_time[0].split('-');
+  //Load data
+ useEffect(() => {
+    sendRequest().then(data=>{
+      const initialData = JSON.parse(data);
+        for (let i = 0; i< initialData.length;i++) {
+        var startdate_and_time = initialData[i][3].split(' '); 
+        var startdate_time_split = startdate_and_time[0].split('-');
 
-           var enddate_and_time = initialData[i][4].split(' ');
-           var enddate_time_split = enddate_and_time[0].split('-');
+        var enddate_and_time = initialData[i][4].split(' ');
+        var enddate_time_split = enddate_and_time[0].split('-');
 
-           var campaign_date =  new Date(startdate_time_split[0], startdate_time_split[1]-1, startdate_time_split[2]); 
-           var end_campaign_date = new Date(enddate_time_split[0],enddate_time_split[1]-1,enddate_time_split[2]);
-        
-            if (campaign_date.getMonth() <= currentdate.getMonth() 
-              && campaign_date.getDate() <= currentdate.getDate() 
-              && campaign_date.getFullYear() <= currentdate.getFullYear() 
-              && currentdate.getMonth() <= end_campaign_date.getMonth()
-              && currentdate.getDate() <= end_campaign_date.getDate()
-              && currentdate.getFullYear() <= end_campaign_date.getFullYear()) {
-                console.log("Campaign is ongoing now!");
-                setCurrentCampaign(initialData[i]);
-                console.log(currentCampaign);
-           }
-         }
+        var campaign_date =  new Date(startdate_time_split[0], startdate_time_split[1]-1, startdate_time_split[2]); 
+        var end_campaign_date = new Date(enddate_time_split[0],enddate_time_split[1]-1,enddate_time_split[2]);
+      
+          if (campaign_date.getMonth() <= currentdate.getMonth() 
+            && campaign_date.getDate() <= currentdate.getDate() 
+            && campaign_date.getFullYear() <= currentdate.getFullYear() 
+            && currentdate.getMonth() <= end_campaign_date.getMonth()
+            && currentdate.getDate() <= end_campaign_date.getDate()
+            && currentdate.getFullYear() <= end_campaign_date.getFullYear()) {
+              console.log("Campaign is ongoing now!");
+              setCurrentCampaign(initialData[i]);
+              console.log(currentCampaign);
+        }
+      }
+    })
+},[])
 
-     }catch(error){
-         console.log(error.message);
-     }
-     }
-     sendRequest();
-     
-},[initialData])
   return (
     <div className="MainDash bg-primary w-full overflow-hidden">
       <Navbar/>
